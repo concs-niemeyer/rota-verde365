dd0d8d3b3e3b58e798df611434b15b82d82dc57e
concs-niemeyer <conrado.raizen@gmail.com>
2025-04-25 15:58:36 -0300

    Corrige a URL da API no método signIn para usar o endpoint correto

--- src/context/auth.jsx
+++ src/context/auth.jsx
@@ -20,7 +20,7 @@ export function AuthProvider({ children }) {
 
   async function signIn({ email, password }) {
     try {
-      const response = await api(`/login?email=${email}`);
+      const response = await api(`/user?email=${email}`);
       console.log(response, "<<<RESPOSTA API>>>")
       const data = await response.json();
 
