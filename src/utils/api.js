75410542b114a45fe91c6302cd860fc6b4f6e1cf
concs-niemeyer <conrado.raizen@gmail.com>
2025-04-26 18:51:26 -0300

    fix: remove dotenv dependency and use import.meta.env for server URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,16 +1,12 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 /**
  * 
  * @param {string} endpoint 
  * @param {RequestInit} init 
  * @returns 
  */
+export function api(endpoint, init) {
+	const serverUrl = import.meta.env.VITE_SERVER_URL; // Aqui é import.meta.env
+	const url = serverUrl + endpoint;
 
-export function api( endpoint, init ) {
-	const serverUrl = process.env.SERVER_URL
-	const url = serverUrl + endpoint
-
-	return fetch(url, init)
+	return fetch(url, init);
 }
