3287caf1c68e89bbba272b4cbc8e6b659c829fcc
concs-niemeyer <conrado.raizen@gmail.com>
2025-04-26 18:31:33 -0300

    fix: add allowedHosts configuration for Vite server

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts:"rota-verde365.onrender.com",
     host: true,
     port: process.env.PORT || 5173
   }
