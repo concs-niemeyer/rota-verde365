55043f347634e3fb8a8eb77b2838017bc11c4455
concs-niemeyer <conrado.raizen@gmail.com>
2025-04-28 23:29:08 -0300

    fix: update Vite configuration to use proxy for API requests

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts:["rota-verde365.onrender.com"],
-    host: true,
-    port: process.env.PORT || 5173
+    proxy:{
+      '/api': "rota-verde365.onrender.com"
+    }
   }
 })
