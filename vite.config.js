c54584d79dfe0b931ce40edfb8b8c7c8c69d1620
concs-niemeyer <conrado.raizen@gmail.com>
2025-04-29 14:51:02 -0300

    fix: update Vite configuration to include CORS settings and correct server port

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy:{
-      '/api': 'rota-verde365.onrender.com'
+    cors: {
+      origin: /^https?:\/\/(?:[^:]+\.)*rota-verde365\.onrender\.com$/,
+      credentials: true,
     },
-    allowedHosts:["rota-verde365.onrender.com"],
+
+    allowedHosts: ["rota-verde365.onrender.com"],
     host: true,
-    port: process.env.PORT || 5173
-  }
-})
+    port: process.env.PORT || 10000,
+  },
+});
+
+// export default defineConfig({
+//   // ...
+//   server: {
+//     // ...
+//     cors: {
+//       origin: /^https?:\/\/(?:[^:]+\.)*bamstore-store\.onrender\.com$/,
+//       credentials: true,
+//     },
+//   },
+//   // ...
+// });
